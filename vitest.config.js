afae8b85f6804f9aba9aacc7b485321fa19cfab6
Lukasz Adamczyk <ladamczyk.it@gmail.com>
2024-08-21 06:47:22 +0200

    chore: adjust

--- vitest.config.js
+++ vitest.config.js
@@ -3,9 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      provider: "istanbul",
       include: ["src/**"],
-      reporter: ["text", "json-summary"],
     },
   },
 });
